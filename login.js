78155188042f9d9f7f398cf60935910c3d4332ab
이훤 <hubo0217@naver.com>
2025-04-23 18:14:08 +0900

    업데잍,

--- login.js
+++
@@ -1,5 +0,0 @@
-function login() {
-  const id = document.getElementById("login-id").value;
-  const pw = document.getElementById("login-password").value;
-  alert(`로그인 시도: 아이디=${id}, 비밀번호=${pw}`);
-}
--- main.js
+++
@@ -1,7 +0,0 @@
-function goToLogin() {
-  window.location.href = "login.html";
-}
-
-function goToSignup() {
-  window.location.href = "signup.html";
-}
